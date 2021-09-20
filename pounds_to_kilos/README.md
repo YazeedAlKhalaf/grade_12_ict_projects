dd4bb0a6e3a3bc2eaf909475094f26c04407d08c
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-09-20 22:32:36 +0300

    ➕ Add pounds to kilos

---
+++ pounds_to_kilos/README.md
@@ -0,0 +1,3 @@
+# pounds to kilos
+
+<img src="https://raw.githubusercontent.com/YazeedAlKhalaf/grade_12_ict_projects/main/pounds_to_kilos/pounds_to_kilos_readme_image.png" width="400px" />
---
+++ pounds_to_kilos/main.py
@@ -0,0 +1,9 @@
+print("This calculator will convert pounds to kilos in a breeze.")
+print("By: @YazeedAlKhalaf")
+print("\n")
+
+pounds = int(input("Weight in pounds: "))
+
+kilos = pounds / 2.205
+
+print("Weight in kilos: ", kilos)
